8a69cedcc078a5e1319a5a6261bb70c5cf7dd7fc
Allie Zhao <allie.k.zhao@gmail.com>
2022-08-09 18:47:11 -0700

    title functionality

--- app.js
+++ app.js
@@ -2,7 +2,20 @@
 
 // let state
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+// set event listeners
+// get user input
+// use user input to update state
+// update DOM to reflect the new state
+
+document.title = 'what is this';
+
+//
+const heading = document.getElementById('heading');
+heading.textContent = 'screeeee';
+
+//set nameInput to reference to html element with ID 'name'
+const nameInput = document.getElementById('name');
+//listen for event to trigger header to change when anything is typed into box
+nameInput.addEventListener('input', () => {
+    heading.textContent = nameInput.value;
+});
