d3ffbb692de6bd5a8803b134914690432afa5630
Allie Zhao <allie.k.zhao@gmail.com>
2022-08-09 20:42:54 -0700

    added name, title, phone num, class, gradient deco

--- app.js
+++ app.js
@@ -1,21 +1,36 @@
 // import functions and grab DOM elements
+//set nameInput to reference to html element with ID 'name'
+const heading = document.getElementById('heading');
+const nameInput = document.getElementById('name');
+const titleInput = document.getElementById('title');
+const phoneNumInput = document.getElementById('phoneNum');
+const classSelect = document.getElementById('class');
+
+const nameOnCard = document.getElementById('nameOnCard');
+const titleOnCard = document.getElementById('titleOnCard');
+const phoneNumOnCard = document.getElementById('phoneNumOnCard');
+const classOnCard = document.getElementById('classOnCard');
 
 // let state
 
 // set event listeners
+//listen for event to trigger header to change when anything is typed into box
+nameInput.addEventListener('input', () => {
+    nameOnCard.textContent = nameInput.value;
+});
+titleInput.addEventListener('input', () => {
+    titleOnCard.textContent = titleInput.value;
+});
+phoneNumInput.addEventListener('input', () => {
+    phoneNumOnCard.textContent = phoneNumInput.value;
+});
+classSelect.addEventListener('change', () => {
+    classOnCard.textContent = classSelect.value;
+});
+
 // get user input
 // use user input to update state
 // update DOM to reflect the new state
 
-document.title = 'what is this';
-
-//
-const heading = document.getElementById('heading');
-heading.textContent = 'screeeee';
-
-//set nameInput to reference to html element with ID 'name'
-const nameInput = document.getElementById('name');
-//listen for event to trigger header to change when anything is typed into box
-nameInput.addEventListener('input', () => {
-    heading.textContent = nameInput.value;
-});
+document.title = 'Business Card Maker';
+heading.textContent = 'Business Card Maker';
